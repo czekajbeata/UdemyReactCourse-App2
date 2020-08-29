2cfeef478d00addb0e6b764c785c1cbbd8c3d30f
Beata <beata.czekaj.7@gmail.com>
2020-08-29 18:03:54 +0200

    using state in class component

--- src/index.js
+++ src/index.js
@@ -1,8 +1,35 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-const App = () => {
-  return <div>Hi there!</div>;
-};
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      latitude: null,
+      errorMessage: "",
+    };
+
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => {
+        this.setState({ latitude: position.coords.latitude });
+      },
+      (error) => {
+        this.setState({ errorMessage: error.message });
+      }
+    );
+  }
+
+  render() {
+    if (this.state.errorMessage && !this.state.latitude) {
+      return <div>Error: {this.state.errorMessage}</div>;
+    }
+
+    if (!this.state.errorMessage && this.state.latitude) {
+      return <div>Latitude: {this.state.latitude}</div>;
+    }
+
+    return <div>Loading...</div>;
+  }
+}
 
 ReactDOM.render(<App />, document.getElementById("root"));
