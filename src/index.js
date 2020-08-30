12439af95a89aaac4a8a41fe4376aa6eb1fb737e
Beata <beata.czekaj.7@gmail.com>
2020-08-30 12:06:04 +0200

    added Loader component

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import SeasonDisplay from "./SeasonDisplay";
+import Loader from "./Loader";
 
 class App extends React.Component {
   state = {
@@ -24,7 +25,7 @@ class App extends React.Component {
       return <SeasonDisplay latitude={this.state.latitude} />;
     }
 
-    return <div>Loading...</div>;
+    return <Loader />;
   }
 }
 
